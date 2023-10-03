986cafa373c4867d9bcefee0a80c32950e89b85f
Dell <usamailyas007>
2023-10-03 05:39:02 -0700

    final commit 1

---
+++ lib/Model/profileModel.dart
@@ -0,0 +1,13 @@
+class ProfileModel {
+  final String profileImageUrl;
+  final String username;
+  final String email;
+
+  ProfileModel({
+    required this.profileImageUrl,
+    required this.username,
+    required this.email,
+  });
+
+// Other class methods and constructors...
+}
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'Model/profileModel.dart';
 
 
 class ProfileScreen extends StatefulWidget {
@@ -10,6 +15,32 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  // Future<List<Profilemodel>> getProfileApi () async{
+  //   final response = await http.get(Uri.parse('https://fluter-otp-server-api.cyclic.app/api/users/get_all_users'));
+  //   var data = jsonDecode(response.body.toString());
+  //   if(response.statusCode == 200 ){
+  //     print(data);
+  //     return Profilemodel.fromJson(data);
+  //   } else {
+  //     return Profilemodel.fromJson(data);
+  //   }
+  // }
+  final List<ProfileModel> profileDataList = [];
+  Future<List<ProfileModel>> getProfileApi ()async{
+    final response = await http.get(Uri.parse('https://fluter-otp-server-api.cyclic.app/api/users/get_all_users'));
+    var data = jsonDecode(response.body.toString());
+
+    print(data);
+    if(response.statusCode == 200){
+      for(Map i in data){
+        ProfileModel userData = ProfileModel(profileImageUrl: i['profileImageUrl'], username: i['username'], email: i['email']);
+        profileDataList.add(userData);
+      }
+      return profileDataList;
+    }else {
+      return profileDataList;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,112 +53,132 @@ class _ProfileScreenState extends State<ProfileScreen> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: width * 0.04,top: height * 0.06,bottom: height * 0.007),
-              child: Row(
-                children: [
-                  const Icon(Icons.arrow_back_ios,size: 20,),
-                  SizedBox(width: width * 0.05),
-                  Text('Settings',style: GoogleFonts.poppins(fontSize:22,fontWeight:FontWeight.w700,color: Colors.black,)),
-                ]
-              ),
-            ),
-            Container(
-              height: height * 0.03,
-              color: const Color(0x66C4C4C4),
-              child: Padding(
-                padding: EdgeInsets.only(left: width * 0.04),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: width * 0.04,top: height * 0.06,bottom: height * 0.007),
                 child: Row(
-                  children: [
-                    Center(child: Text('My Account',style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w700,color: Color(0xFF2743FD)))),
-                  ],
+                    children: [
+                      const Icon(Icons.arrow_back_ios,size: 20,),
+                      SizedBox(width: width * 0.05),
+                      Text('Settings',style: GoogleFonts.poppins(fontSize:22,fontWeight:FontWeight.w700,color: Colors.black,)),
+                    ]
                 ),
               ),
-            ),
-            SizedBox(height: height * 0.035),
-            const CircleAvatar(
-              radius: 45,
-              backgroundImage: AssetImage('assets/images/img.png'),
-            ),
-            SizedBox(height: height * 0.04),
-            Padding(
-              padding: EdgeInsets.only(left: width * 0.04,right: width * 0.06,top: height * 0.01,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Display Name',style: GoogleFonts.poppins(fontSize:12,fontWeight:FontWeight.w400,color: Colors.black)),
-                  Text('Jenny',style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
-                ],
+              Container(
+                height: height * 0.03,
+                color: const Color(0x66C4C4C4),
+                child: Padding(
+                  padding: EdgeInsets.only(left: width * 0.04),
+                  child: Row(
+                    children: [
+                      Center(child: Text('My Account',style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w700,color: Color(0xFF2743FD)))),
+                    ],
+                  ),
+                ),
               ),
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            Padding(
-            padding: EdgeInsets.only(left: width * 0.04,right: width * 0.06,top: height * 0.01,),
-            child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Mobile Number',style: GoogleFonts.poppins(fontSize:12,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
-              Text('123456789',style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
-            ],
-          ),
-        ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Privacy Settings',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Security Settings',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Two-Factor-Authentication',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Connected Social Media',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Notification Preferences',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'App Theme and Personlization',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-            CustomRow(
-              text: 'Languages',
-              ontap: (){},
-            ),
-            const Divider(
-              color: Color(0xFFC4C4C4),
-            ),
-          ]
+              SizedBox(height: height * 0.035),
+              FutureBuilder(
+                future: getProfileApi(),
+                builder: (context,AsyncSnapshot<List<ProfileModel>>  snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else{
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                         CircleAvatar(
+                          radius: 45,
+                          backgroundImage: NetworkImage(snapshot.data![index].profileImageUrl.toString()),
+                        ),
+                        SizedBox(height: height * 0.04),
+                        Padding(
+                          padding: EdgeInsets.only(left: width * 0.04,right: width * 0.06,top: height * 0.01,),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Display Name',style: GoogleFonts.poppins(fontSize:12,fontWeight:FontWeight.w400,color: Colors.black)),
+                              Text(snapshot.data![index].username.toString(),style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          color: Color(0xFFC4C4C4),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: width * 0.04,right: width * 0.06,top: height * 0.01,),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Email',style: GoogleFonts.poppins(fontSize:12,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
+                              Text(snapshot.data![index].email.toString(),style: GoogleFonts.poppins(fontSize:10,fontWeight:FontWeight.w400,color: Color(0xFF565656))),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },);
+                }
+              },),
+
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Privacy Settings',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Security Settings',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Two-Factor-Authentication',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Connected Social Media',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Notification Preferences',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'App Theme and Personlization',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+              CustomRow(
+                text: 'Languages',
+                ontap: (){},
+              ),
+              const Divider(
+                color: Color(0xFFC4C4C4),
+              ),
+            ]
         ),
       )
     );
@@ -157,3 +208,4 @@ class CustomRow extends StatelessWidget {
     );
   }
 }
+
